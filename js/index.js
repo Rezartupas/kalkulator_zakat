916b66a0886818d0cad37051542b2140cff69cca
Reza Saputra <rezagondol@gmail.com>
2024-09-05 13:49:14 +0700

    update final

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ document.getElementById('zakatForm').addEventListener('submit', function(e) {
     const zakatType = document.getElementById('jenisZakat').value;
   
     let zakatAmount = 0;
+    let hartaBersih = 0;
     const hargaEmas = 1247259; // Harga emas per gram
   
     // Menghitung zakat maal berdasarkan jenis zakat yang dipilih
@@ -16,11 +17,20 @@ document.getElementById('zakatForm').addEventListener('submit', function(e) {
     } else if (zakatType === 'perdagangan') {
       zakatAmount = (totalAssets - expenses) * 0.025;
     }
-  
+ 
+    if (zakatType === 'penghasilan' || zakatType === 'tabungan') {
+        hartaBersih = (totalAssets - expenses) - 0.025;
+      } else if (zakatType === 'emas') {
+        hartaBersih = (totalAssets / hargaEmas) - 0.025 * hargaEmas;
+      } else if (zakatType === 'perdagangan') {
+        hartaBersih = (totalAssets - expenses) - 0.025;
+      }
+
     // Cek apakah zakatAmount kurang dari nisab (85 kali harga emas)
     const nisab = hargaEmas * 85;
-    if (zakatAmount < nisab) {
+    if (hartaBersih < nisab) {
       alert("Belum wajib zakat nih, sedekah aja ya?");
+      window.location.href = "https://salingberbagi.or.id";
     } else {
       // Update hasil perhitungan jika memenuhi syarat zakat
       document.getElementById('zakatAmount').textContent = zakatAmount.toLocaleString() + " IDR";
